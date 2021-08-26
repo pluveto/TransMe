4876e712455060a6f5172cfcaa05d7966b13bfbc
pluveto <receding@protonmail.com>
2021-08-27 00:05:26 +0800

    feat: 支持多行翻译

--- TransMe/Translator.cs
+++ TransMe/Translator.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -14,16 +17,15 @@ namespace TransMe
         {
             var toLanguage = "zh";
             var fromLanguage = "en";
-            var url = $"https://translate.googleapis.com/translate_a/single?client=gtx&sl={fromLanguage}&tl={toLanguage}&dt=t&q={HttpUtility.UrlEncode(Word)}";
+            var url = $"https://translate.googleapis.com/translate_a/single?client=gtx&sl={fromLanguage}&tl={toLanguage}&dt=t&q={Uri.EscapeDataString(Word)}";
             var webClient = new WebClient
             {
                 Encoding = System.Text.Encoding.UTF8
             };
             var result = await webClient.DownloadStringTaskAsync(url);
             try
-            {
-                result = result.Substring(4, result.IndexOf("\"", 4, StringComparison.Ordinal) - 4);
-                return result;
+            {                              
+                return Parse(result);
             }
             catch
             {
@@ -31,5 +33,52 @@ namespace TransMe
             }
 
         }
+
+        private string Parse(string result)
+        {
+            // Ref: https://github.com/wadereye/GoogleTranslateFreeApi/blob/master/GoogleTranslateFreeApi/GoogleTranslator.cs
+            JToken tmp = JsonConvert.DeserializeObject<JToken>(result);
+            string originalTextTranscription = null, translatedTextTranscription = null;
+
+            GetMainTranslationInfo(tmp[0], out var translation,
+                ref originalTextTranscription, ref translatedTextTranscription);
+            var mainTranslationInfo = tmp[0];
+            return string.Join('\n', translation);
+
+        }
+
+        protected static void GetMainTranslationInfo(JToken translationInfo, out string[] translate,
+            ref string originalTextTranscription, ref string translatedTextTranscription)
+        {
+            List<string> translations = new List<string>();
+
+            foreach (var item in translationInfo)
+            {
+                if (item.Count() >= 5)
+                    translations.Add(item.First.Value<string>());
+                else
+                {
+                    var transcriptionInfo = item;
+                    int elementsCount = transcriptionInfo.Count();
+
+                    if (elementsCount == 3)
+                    {
+                        translatedTextTranscription = (string)transcriptionInfo[elementsCount - 1];
+                    }
+                    else
+                    {
+                        if (transcriptionInfo[elementsCount - 2] != null)
+                            translatedTextTranscription = (string)transcriptionInfo[elementsCount - 2];
+                        else
+                            translatedTextTranscription = (string)transcriptionInfo[elementsCount - 1];
+
+                        originalTextTranscription = (string)transcriptionInfo[elementsCount - 1];
+                    }
+                }
+            }
+
+            translate = translations.ToArray();
+        }
+
     }
 }
